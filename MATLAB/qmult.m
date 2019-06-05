4ac4c60ca10dee0f45a9c39a42672326c7fbb5a2
Marco Hinojosa <37319168+marco-hinojosa@users.noreply.github.com>
2019-06-05 14:57:18 -0400

    Update qmult.m

--- MATLAB/qmult.m
+++ MATLAB/qmult.m
@@ -3,5 +3,5 @@ function qhat = qmult(q)
 % Convert Quaternion into a 4x4 Square Matrix
 v = q(1:3);
 s = q(4);
-qhat = [s*eye(3) + hat(v)  v ; -v' s]; 
+qhat = [s*eye(3) + vec2skew(v)  v ; -v' s]; 
 end
