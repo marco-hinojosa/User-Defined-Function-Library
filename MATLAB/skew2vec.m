dbf0c49b5e3916a6fd574c166c8492552d7c4e4e
Marco Hinojosa <37319168+marco-hinojosa@users.noreply.github.com>
2019-06-05 15:20:41 -0400

    Update skew2vec.m

--- MATLAB/skew2vec.m
+++ MATLAB/skew2vec.m
@@ -1,8 +1,13 @@
 function v = unhat(M)
-% Unhat Operator Function
-% Converts a Skew Symmetric Matrix into a Vector
+% Unhat Operator Function: Converts a Skew Symmetric Matrix into a Vector
+%
+% Inputs:
+%       M - 3x3 Skew-Symmetric Matrix
+%
+% Outputs:
+%       v - 3x1 Vector
+
 v = zeros(3,1);
 v(1) = -M(2,3);
 v(2) = M(1,3);
 v(3) = -M(1,2);
-end
