d5d0e8086b37577d97f9cb9be4f1cf6fcf877f46
Marco Hinojosa <37319168+marco-hinojosa@users.noreply.github.com>
2019-06-05 15:10:38 -0400

    Update q2rot.m

--- MATLAB/q2rot.m
+++ MATLAB/q2rot.m
@@ -3,8 +3,10 @@ function [Q] = q2rot(q)
 %
 % Inputs:
 %       q - 4x1 Scalar-Last Quaternion
+%
+% Outputs:
+%       Q - 3x3 Rotation Matrix
+
 v = q(1:3);
 s = q(4);
-
-Q = eye(3) + 2*hat(v)*(s*eye(3) + hat(v));
-end
+Q = eye(3) + 2*vec2skew(v)*(s*eye(3) + vec2skew(v));
