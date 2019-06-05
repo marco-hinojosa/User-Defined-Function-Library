2f54423450e8a747e2452b22ee4c7e68502063c2
Marco Hinojosa <37319168+marco-hinojosa@users.noreply.github.com>
2019-06-05 15:35:56 -0400

    Update tvlqr.m

--- MATLAB/tvlqr.m
+++ MATLAB/tvlqr.m
@@ -1,6 +1,16 @@
 function [K,S] = tvlqr(A,B,Q,R,S)
-%TVLQR Time-Varying Linear-Quadratic Regulator design from continuous 
-%      cost function.
+% Time-Varying Linear-Quadratic Regulator for Continuous Cost Function.
+%
+% Inputs:
+%       A - State Matrix
+%       B - Input Matrix
+%       Q - State Cost
+%       R - Control Cost
+%       S - "Final" State Cost
+%
+% Outputs:
+%       K - TVLQR Gains
+%       S - Updated "Final" State Cost (Backwards Integration)
 
 K = inv(R + B'*S*B)*B'*S*A;
 S = Q + K'*R*K + (A - B*K)'*S*(A - B*K);
