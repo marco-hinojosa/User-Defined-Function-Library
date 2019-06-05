da371ca20642086cf7a6fc6defa608d7d1d1aa8d
Marco Hinojosa <37319168+marco-hinojosa@users.noreply.github.com>
2019-06-05 15:21:39 -0400

    Update vec2skew.m

--- MATLAB/vec2skew.m
+++ MATLAB/vec2skew.m
@@ -1,6 +1,12 @@
 function M = hat(v)
-% Hat Operator Function
-% Converts a Vector into a Skew Symmetric Matrix
+% Hat Operator Function: Converts a Vector into a Skew Symmetric Matrix
+%
+% Inputs:
+%       v - 3x1 Vector
+%
+% Outputs:
+%       M - 3x3 Skew-Symmetric Matrix
+
 M = zeros(3,3);
 M(1,2) = -v(3);
 M(2,1) = v(3);
@@ -8,5 +14,3 @@ M(1,3) = v(2);
 M(3,1) = -v(2);
 M(2,3) = -v(1);
 M(3,2) = v(1);
-end
-
