6e084d242ba408e24a243f7686b1b6fc69295f90
Marco Hinojosa <37319168+marco-hinojosa@users.noreply.github.com>
2019-06-05 15:06:36 -0400

    Update q2axis.m

--- MATLAB/q2axis.m
+++ MATLAB/q2axis.m
@@ -1,7 +1,11 @@
 function phi = q2axis(q)
 % Converts Rotation Matrix Q into Quaternion q
-% Input - q Quaternion Matrix 
-% Output - phi Axis Angle
+%
+% Inputs:
+%       q - 4x1 Scalar-Last Quaternion
+%
+% Outputs:
+%       phi - 3x1 Axis Angle Vector [rad]
+
 Q = q2rot(q);
 phi = skew2vec(logm(Q));
-end
