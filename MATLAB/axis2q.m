cb7346dae6b7e5166f158e9bf69caf9a19134d20
Marco Hinojosa <37319168+marco-hinojosa@users.noreply.github.com>
2019-06-05 15:01:36 -0400

    Update axis2q.m

--- MATLAB/axis2q.m
+++ MATLAB/axis2q.m
@@ -1,7 +1,13 @@
-function q = phi2q(phi)
-% Converts Axis-Angle Vector phi into Quaternion q
-% Input - phi Axis Angle 
-% Output - q Quaternion Matrix
+function [q] = axis2q(phi)
+% Converts Axis-Angle Vector into Quaternion
+%
+% Inputs:
+%       phi - 3x1 Axis-Angle Vector
+%
+% Outputs:
+%       q - 4x1 Scalar-Last Quaternion
+
+% For Large Angles
 if norm(phi) > 10*pi/180 
     theta = norm(phi);
     r = phi/theta;
@@ -9,8 +15,8 @@ if norm(phi) > 10*pi/180
     q(1:3) = r.*sin(theta/2);
     q(4) = cos(theta/2);
     q = q';
-else % Small Angle Approximation
+% For Small Angle Approximation
+else
     q = [phi./2 ; 1 - 1/8*(phi'*phi)];
     q = q/norm(q);
-end
 end
