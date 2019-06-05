2119b729373c02b5de103ee35a83233cd5e06179
Marco Hinojosa <37319168+marco-hinojosa@users.noreply.github.com>
2019-06-05 15:12:06 -0400

    Update rot2q.m

--- MATLAB/rot2q.m
+++ MATLAB/rot2q.m
@@ -1,9 +1,15 @@
-function q = rot2q(Q)
-% Converts Rotation Matrix Q into Quaternion q
-% Input - Q Rotation Matrix 
-% Output - q Quaternion
-phi = unhat(logm(Q));
+function [q] = rot2q(Q)
+% Converts Rotation Matrix into Quaternion
+%
+% Inputs:
+%       Q - 3x3 Rotation Matrix
+%
+% Outputs:
+%       q - 4x1 Scalar-Last Quaternion
+
+phi = skew2vec(logm(Q));
 theta = norm(phi);
+
 if theta ~= 0
     r = phi/theta;
     q = [r*sin(theta/2); cos(theta/2)];
