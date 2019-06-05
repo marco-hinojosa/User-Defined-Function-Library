f96068b4ad7b9c4ce67d996cadd5ac386f7134e5
Marco Hinojosa <37319168+marco-hinojosa@users.noreply.github.com>
2019-06-05 15:15:32 -0400

    Update qconj.m

--- MATLAB/qconj.m
+++ MATLAB/qconj.m
@@ -2,10 +2,10 @@ function [q] = qconj(qq)
 % Quaternion Conjugate Operator
 %
 % Inputs:
-%       qq - 4x1 Input Quaternion
+%       qq - 4x1 Input Scalar-Last Quaternion
 %
 % Outputs:
-%       q - 4x1 Output Quaternion
+%       q - 4x1 Output Scalar-Last Quaternion
 
 v = qq(1:3);
 v = -v;
