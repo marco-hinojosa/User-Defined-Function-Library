7807d63cea61e5ce7249c1b181ea4b16010182a3
Marco Hinojosa <37319168+marco-hinojosa@users.noreply.github.com>
2019-06-05 15:04:13 -0400

    Update keplerEq.m

--- MATLAB/keplerEq.m
+++ MATLAB/keplerEq.m
@@ -1,10 +1,17 @@
 function E = keplerEq(M,e,eps)
-% Function solves Kepler's equation M = E-e*sin(E)
-% Input - Mean anomaly M [rad] , Eccentricity e and Epsilon 
-% Output  eccentric anomaly E [rad]. 
-En  = M;
+% Kepler's Equation Solver: M = E - e*sin(E)
+%
+% Inputs:
+%	M - Mean Anomaly [rad]
+%	e - Eccentricity
+%	eps - Error Tolerance
+%
+% Outputs:
+%	E - Eccentric Anomaly [rad]
+
+En = M;
 Ens = En - (En-e*sin(En)- M)/(1 - e*cos(En));
-while ( abs(Ens-En) > eps )
+while abs(Ens-En)>eps
 	En = Ens;
 	Ens = En - (En - e*sin(En) - M)/(1 - e*cos(En));
 end
