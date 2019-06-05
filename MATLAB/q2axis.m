20c509a8d4206b82e68b9f8413a4a7a874db8dfe
Marco Hinojosa <37319168+marco-hinojosa@users.noreply.github.com>
2019-06-05 14:55:29 -0400

    Update q2axis.m

--- MATLAB/q2axis.m
+++ MATLAB/q2axis.m
@@ -2,6 +2,6 @@ function phi = q2axis(q)
 % Converts Rotation Matrix Q into Quaternion q
 % Input - q Quaternion Matrix 
 % Output - phi Axis Angle
-Q = quat2Q(q);
+Q = q2rot(q);
 phi = unhat(logm(Q));
 end
