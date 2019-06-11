edf1293564adbe0d39f5179ef5137d426ef5ca2f
Marco Hinojosa-Garcia <37319168+marco-hinojosa@users.noreply.github.com>
2019-06-11 11:42:59 -0400

    Added notes to icrf rotation

---
+++ MATLAB/icrf2itrf.m
@@ -0,0 +1,15 @@
+function rot = icrf2itrf(gmst)
+% crf2trf Creates a rotation  matrix  between  CRF  and  TRF systems
+% Notes: GMST is the total angle of rotation about Earth's axis since the 
+%        epoch January 1, 2000 12:00h, which is what the International 
+%        Celestial Reference Frame (ICRF) is based upon. This function does
+%        not consider perturbation motions (nutation, precession, polar
+%        motion, equation of equinoxes, etc.)
+%
+% Inputs:
+%       gmst - Greenwich  Mean  Sidereal  Time [rad]
+%
+% Outputs:
+%       rot - Rotation Matrix from Celestial to Terrestrial Frames
+
+rot = rotz(-gmst);
